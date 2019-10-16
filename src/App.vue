54cbf46f3a8054c30c6b94253763e57fde1d0359
184024871 <184024871@qq.com>
2019-10-16 23:41:03 +0800

    淘淘商城main

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <keep-alive exclude="classify,detail">
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+  @import './base/css/normalize.scss';
+  @import url('//at.alicdn.com/t/font_342152_sf25eoela9f9lik9.css');
+  body{
+    background: #f1f8f7;
+    #app{
+      background: #fff;
+      height:100%;
+      font-family: Microsoft YaHei;
+    }
+    .swiper-pagination-bullet{
+      width:8px;
+      height:8px;
+      [data-dpr="2"] & {
+        width:16px;
+        height:16px;
+      }
+      [data-dpr="3"] & {
+        width:24px;
+        height:24px;
+      }
+    }
+  }
 </style>
